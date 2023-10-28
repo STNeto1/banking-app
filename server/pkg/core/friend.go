ebdf20b0feecdd5f63f222ddd5e7397641037e6d
STNeto1 <gfreitasneto18@gmail.com>
2023-10-27 22:50:17 -0300

    delete friend

--- server/pkg/core/friend.go
+++ server/pkg/core/friend.go
@@ -2,10 +2,15 @@ package core
 
 import (
 	"context"
+	"fmt"
 	"log"
 
 	"github.com/huandu/go-sqlbuilder"
 	"github.com/jmoiron/sqlx"
+)
+
+var (
+	ErrFriendDoesNotExist = fmt.Errorf("Friend does not exist")
 )
 
 type FriendContainer struct {
@@ -18,7 +23,6 @@ func NewFriendContainer(connection *sqlx.DB) *FriendContainer {
 	}
 }
 
-// list your friends
 func (fc *FriendContainer) GetFriends(ctx context.Context, userID string) ([]User, error) {
 	sb := sqlbuilder.PostgreSQL.NewSelectBuilder().From("friends")
 
@@ -50,4 +54,56 @@ func (fc *FriendContainer) GetFriends(ctx context.Context, userID string) ([]Use
 	return users, nil
 }
 
-// delete friend
+func (fc *FriendContainer) DeleteFriend(ctx context.Context, userID, friendID string) error {
+	tx, err := fc.connection.BeginTxx(ctx, nil)
+	if err != nil {
+		log.Println("error creating transaction", err)
+
+		return ErrInternalError
+	}
+
+	sb := sqlbuilder.PostgreSQL.NewSelectBuilder().From("friends")
+	_sql, args := sb.Select("count(*)").
+		Where(sb.Equal("user_id", userID), sb.Equal("friend_id", friendID)).
+		Build()
+
+	row := tx.QueryRowxContext(ctx, _sql, args...)
+	if err != nil {
+		log.Println("failed to query", err)
+
+		return ErrInternalError
+	}
+	var count int
+	if err := row.Scan(&count); err != nil {
+		log.Println("error scanning friends count", err)
+
+		rollbackx(tx)
+		return ErrInternalError
+	}
+
+	if count == 0 {
+		rollbackx(tx)
+		return ErrFriendDoesNotExist
+	}
+
+	db := sqlbuilder.PostgreSQL.NewDeleteBuilder().DeleteFrom("friends")
+	_sql, args = db.
+		Where(db.Equal("user_id", userID), db.Equal("friend_id", friendID)).
+		Build()
+
+	_, err = tx.ExecContext(ctx, _sql, args...)
+	if err != nil {
+		log.Println("error deleting row", err)
+
+		rollbackx(tx)
+		return ErrInternalError
+	}
+
+	if err := tx.Commit(); err != nil {
+		log.Println("error commiting transaction", err)
+
+		return ErrInternalError
+	}
+
+	return nil
+}
--- server/pkg/core/friend_test.go
+++ server/pkg/core/friend_test.go
@@ -38,3 +38,30 @@ func TestListUserFriends(t *testing.T) {
 	assert.Len(t, friends, 1)
 	assert.NoError(t, err)
 }
+
+func TestDeleteFriend(t *testing.T) {
+	db := core.CreateTempDB()
+	defer db.Close()
+
+	authContainer := core.NewAuthContainer(db)
+	friendContainer := core.NewFriendContainer(db)
+
+	fromUser, err := authContainer.CreateUser(context.Background(), "foo", "mail@mail.com", "102030")
+	assert.NotNil(t, fromUser)
+	assert.NoError(t, err)
+
+	toUser, err := authContainer.CreateUser(context.Background(), "foo", "mail2@mail.com", "102030")
+	assert.NotNil(t, toUser)
+	assert.NoError(t, err)
+
+	sb := sqlbuilder.PostgreSQL.NewInsertBuilder().InsertInto("friends")
+	_sql, args := sb.Cols("id", "user_id", "friend_id").
+		Values(ulid.Make().String(), fromUser.ID, toUser.ID).
+		Build()
+
+	_, err = db.ExecContext(context.Background(), _sql, args...)
+	assert.NoError(t, err)
+
+	err = friendContainer.DeleteFriend(context.Background(), fromUser.ID, toUser.ID)
+	assert.NoError(t, err)
+}
