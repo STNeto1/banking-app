1109291c0a796331e1bd010c3c63bbfe6d1b3715
STNeto1 <gfreitasneto18@gmail.com>
2023-11-10 10:49:41 -0300

    add transfer

--- server/pkg/core/event.go
+++ server/pkg/core/event.go
@@ -128,3 +128,75 @@ func (ec *EventContainer) CreateWithdrawalEvent(ctx context.Context, userID stri
 	}
 	return nil
 }
+
+func (ec *EventContainer) CreateTransferEvent(ctx context.Context, fromUserID, toUserID string, amount decimal.Decimal) error {
+	tx, err := ec.connection.BeginTxx(ctx, &sql.TxOptions{
+		Isolation: sql.LevelSerializable,
+	})
+	if err != nil {
+		log.Println("failed to open transaction", err)
+		return ErrInternalError
+	}
+
+	sb := sqlbuilder.NewSelectBuilder().From("users")
+	_sql, args := sb.Select("balance").Where(sb.Equal("id", fromUserID)).Build()
+
+	var fromUserBalance decimal.Decimal
+	row := tx.QueryRowxContext(ctx, _sql, args...)
+	if err := row.Scan(&fromUserBalance); err != nil {
+		rollbackx(tx)
+
+		log.Println("error fetching user balance", err)
+		return ErrInternalError
+
+	}
+
+	if fromUserBalance.LessThan(amount) {
+		rollbackx(tx)
+
+		return ErrInsufficientBalance
+	}
+
+	ub := sqlbuilder.NewUpdateBuilder().Update("users")
+	_sql, args = ub.Set(ub.Sub("balance", amount)).Where(ub.Equal("id", fromUserID)).Build()
+
+	_, err = tx.ExecContext(ctx, _sql, args...)
+	if err != nil {
+		rollbackx(tx)
+
+		log.Println("error updating balance", err)
+		return ErrInternalError
+	}
+
+	ub = sqlbuilder.NewUpdateBuilder().Update("users")
+	_sql, args = ub.Set(ub.Add("balance", amount)).Where(ub.Equal("id", toUserID)).Build()
+
+	_, err = tx.ExecContext(ctx, _sql, args...)
+	if err != nil {
+		rollbackx(tx)
+
+		log.Println("error updating balance", err)
+		return ErrInternalError
+	}
+
+	cb := sqlbuilder.NewInsertBuilder().InsertInto("events")
+	_sql, args = cb.Cols("id", "user_id", "type", "description", "amount").
+		Values(ulid.Make().String(), fromUserID, "transference_from", "WIP", amount).
+		Values(ulid.Make().String(), toUserID, "transference_to", "WIP", amount).
+		Build()
+
+	_, err = tx.ExecContext(ctx, _sql, args...)
+	if err != nil {
+		rollbackx(tx)
+
+		log.Println("error creating event", err)
+		return ErrInternalError
+	}
+
+	if err := tx.Commit(); err != nil {
+		log.Println("failed to commit", err)
+
+		return ErrInternalError
+	}
+	return nil
+}
--- server/pkg/core/event_test.go
+++ server/pkg/core/event_test.go
@@ -132,6 +132,7 @@ func TestCreateMultipleWithdrawalsWithSuccess(t *testing.T) {
 	}
 
 	wg.Wait()
+
 	updatedUser, err := userContainer.GetUserByID(context.Background(), usr.ID)
 	assert.NotNil(t, updatedUser)
 	assert.NoError(t, err)
@@ -139,4 +140,129 @@ func TestCreateMultipleWithdrawalsWithSuccess(t *testing.T) {
 	assert.True(t, updatedUser.Balance.Equal(decimal.NewFromInt(100)))
 
 	// add 200 to balance, then create 10 "parallel" withdrawal operations with 10 for each of them, resulting in 100 as final balance
+}
+
+func TestMakeTransferWithSuccess(t *testing.T) {
+	db := core.CreateTempDB()
+	defer db.Close()
+
+	authContainer := core.NewAuthContainer(db)
+	eventContainer := core.NewEventContainer(db)
+	userContainer := core.NewUserContainer(db)
+
+	fromUsr, err := authContainer.CreateUser(context.Background(), "foo", "mail@mail.com", "102030")
+	assert.NotNil(t, fromUsr)
+	assert.NoError(t, err)
+
+	toUsr, err := authContainer.CreateUser(context.Background(), "foo", "mail2@mail.com", "102030")
+	assert.NotNil(t, toUsr)
+	assert.NoError(t, err)
+
+	err = eventContainer.CreateDepositEvent(context.Background(), fromUsr.ID, decimal.NewFromInt(10))
+	assert.NoError(t, err)
+
+	err = eventContainer.CreateDepositEvent(context.Background(), toUsr.ID, decimal.NewFromInt(10))
+	assert.NoError(t, err)
+
+	err = eventContainer.CreateTransferEvent(context.Background(), fromUsr.ID, toUsr.ID, decimal.NewFromInt(10))
+	assert.NoError(t, err)
+
+	updatedUser, err := userContainer.GetUserByID(context.Background(), fromUsr.ID)
+	assert.NotNil(t, updatedUser)
+	assert.NoError(t, err)
+	assert.True(t, updatedUser.Balance.Equal(decimal.NewFromInt(0)))
+
+	updatedUser, err = userContainer.GetUserByID(context.Background(), toUsr.ID)
+	assert.NotNil(t, updatedUser)
+	assert.NoError(t, err)
+	assert.True(t, updatedUser.Balance.Equal(decimal.NewFromInt(20)))
+}
+
+func TestMakeTransferWithInsufficientBalance(t *testing.T) {
+	db := core.CreateTempDB()
+	defer db.Close()
+
+	authContainer := core.NewAuthContainer(db)
+	eventContainer := core.NewEventContainer(db)
+	userContainer := core.NewUserContainer(db)
+
+	fromUsr, err := authContainer.CreateUser(context.Background(), "foo", "mail@mail.com", "102030")
+	assert.NotNil(t, fromUsr)
+	assert.NoError(t, err)
+
+	toUsr, err := authContainer.CreateUser(context.Background(), "foo", "mail2@mail.com", "102030")
+	assert.NotNil(t, toUsr)
+	assert.NoError(t, err)
+
+	err = eventContainer.CreateDepositEvent(context.Background(), fromUsr.ID, decimal.NewFromInt(10))
+	assert.NoError(t, err)
+
+	err = eventContainer.CreateDepositEvent(context.Background(), toUsr.ID, decimal.NewFromInt(10))
+	assert.NoError(t, err)
+
+	err = eventContainer.CreateTransferEvent(context.Background(), fromUsr.ID, toUsr.ID, decimal.NewFromInt(15))
+	assert.Equal(t, err, core.ErrInsufficientBalance)
+
+	updatedUser, err := userContainer.GetUserByID(context.Background(), fromUsr.ID)
+	assert.NotNil(t, updatedUser)
+	assert.NoError(t, err)
+	assert.True(t, updatedUser.Balance.Equal(decimal.NewFromInt(10)))
+
+	updatedUser, err = userContainer.GetUserByID(context.Background(), toUsr.ID)
+	assert.NotNil(t, updatedUser)
+	assert.NoError(t, err)
+	assert.True(t, updatedUser.Balance.Equal(decimal.NewFromInt(10)))
+}
+
+func TestMakeMultipleTransferences(t *testing.T) {
+	db := core.CreateTempDB()
+	defer db.Close()
+
+	authContainer := core.NewAuthContainer(db)
+	eventContainer := core.NewEventContainer(db)
+	userContainer := core.NewUserContainer(db)
+
+	fromUsr, err := authContainer.CreateUser(context.Background(), "foo", "mail@mail.com", "102030")
+	assert.NotNil(t, fromUsr)
+	assert.NoError(t, err)
+
+	toUsr, err := authContainer.CreateUser(context.Background(), "foo", "mail2@mail.com", "102030")
+	assert.NotNil(t, toUsr)
+	assert.NoError(t, err)
+
+	err = eventContainer.CreateDepositEvent(context.Background(), fromUsr.ID, decimal.NewFromInt(10))
+	assert.NoError(t, err)
+
+	err = eventContainer.CreateDepositEvent(context.Background(), toUsr.ID, decimal.NewFromInt(10))
+	assert.NoError(t, err)
+
+	count := 10
+	var wg sync.WaitGroup
+	wg.Add(count)
+
+	for i := 0; i < count; i++ {
+		go func(idx int) {
+			defer wg.Done()
+
+			if idx%2 == 0 {
+				err = eventContainer.CreateTransferEvent(context.Background(), fromUsr.ID, toUsr.ID, decimal.NewFromInt(5))
+				assert.NoError(t, err)
+			} else {
+				err = eventContainer.CreateTransferEvent(context.Background(), toUsr.ID, fromUsr.ID, decimal.NewFromInt(5))
+				assert.NoError(t, err)
+			}
+		}(i)
+	}
+
+	wg.Wait()
+
+	updatedUser, err := userContainer.GetUserByID(context.Background(), fromUsr.ID)
+	assert.NotNil(t, updatedUser)
+	assert.NoError(t, err)
+	assert.True(t, updatedUser.Balance.Equal(decimal.NewFromInt(10)))
+
+	updatedUser, err = userContainer.GetUserByID(context.Background(), toUsr.ID)
+	assert.NotNil(t, updatedUser)
+	assert.NoError(t, err)
+	assert.True(t, updatedUser.Balance.Equal(decimal.NewFromInt(10)))
 }
